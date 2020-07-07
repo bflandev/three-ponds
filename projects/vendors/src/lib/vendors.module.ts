11923fcd46ba42b23137cc9f3cebc2e87c902643
Brian Flanagan <bflan@Brians-MBP.fios-router.home>
2020-07-06 21:30:11 -0400

    add material to media project

--- projects/vendors/src/lib/vendors.module.ts
+++ projects/vendors/src/lib/vendors.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { VendorsComponent } from './vendors.component';
 import { A11yModule } from '@angular/cdk/a11y';
 import { ClipboardModule } from '@angular/cdk/clipboard';
 import { DragDropModule } from '@angular/cdk/drag-drop';
@@ -46,9 +45,54 @@ import { MatTreeModule } from '@angular/material/tree';
 
 
 @NgModule({
-  declarations: [VendorsComponent],
+  declarations: [],
   imports: [
   ],
-  exports: [VendorsComponent, MatButtonModule]
+  exports: [
+    A11yModule,
+    ClipboardModule,
+    CdkStepperModule,
+    CdkTableModule,
+    CdkTreeModule,
+    DragDropModule,
+    MatAutocompleteModule,
+    MatBadgeModule,
+    MatBottomSheetModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatStepperModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatDividerModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    MatTreeModule,
+    PortalModule,
+    ScrollingModule,
+
+  ]
 })
 export class VendorsModule { }
