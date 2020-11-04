66ab8dcc811f3dcdb33b664cb4955e165acd0d77
Brian Flanagan <bflan@Brians-MBP.fios-router.home>
2020-11-04 00:07:53 -0500

    temp page

--- projects/digital/src/environments/environment.prod.ts
+++ projects/digital/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBFVMiQomX-wGIHyuCslfx-goo32B0xxdU',
+    authDomain: 'three-ponds-digital.firebaseapp.com',
+    databaseURL: 'https://three-ponds-digital.firebaseio.com',
+    projectId: 'three-ponds-digital',
+    storageBucket: 'three-ponds-digital.appspot.com',
+    messagingSenderId: '479904043239',
+    appId: '1:479904043239:web:d911aa08757e5a87735242',
+    measurementId: 'G-LCZMKBCVQR'
+  }
 };
