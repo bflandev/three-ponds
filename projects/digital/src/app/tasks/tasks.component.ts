c2a7e897c75e852bdec3497edfa9f7b94acca392
Brian Flanagan <bflan@Brians-MBP.fios-router.home>
2020-11-05 17:40:46 -0500

    updated tasks

--- projects/digital/src/app/tasks/tasks.component.ts
+++ projects/digital/src/app/tasks/tasks.component.ts
@@ -1,8 +1,19 @@
 import { CdkDragDrop, transferArrayItem } from '@angular/cdk/drag-drop';
+import { ChangeDetectorRef } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 import { MatDialog } from '@angular/material/dialog';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { TaskDialogComponent, TaskDialogData, TaskDialogResult } from '../task-dialog/task-dialog.component';
 import { Task } from '../task/task.model';
+
+const getObservable = (collection: AngularFirestoreCollection<Task>) => {
+  const subject = new BehaviorSubject([]);
+  collection.valueChanges({idField: 'id'}).subscribe((val: Task[]) => {
+    subject.next(val);
+  });
+  return subject;
+};
 
 @Component({
   selector: 'app-tasks',
@@ -10,21 +21,28 @@ import { Task } from '../task/task.model';
   styleUrls: ['./tasks.component.scss']
 })
 export class TasksComponent implements OnInit {
-  todo: Task[] = [
-    {title: 'Remove Ivy From Barn Path', description: 'Remove the ivy and put in pile near the back deck.'},
-    {title: 'Clear grapevine from canal', description: 'Cut down the grapevine exposing the wall and clear out ivy.'}
-  ];
-  inProgress: Task[] = [];
-  done: Task[] = [];
-  constructor(private dialog: MatDialog) { }
+  todo = getObservable(this.store.collection('todo'));
+  inProgress = getObservable(this.store.collection('inProgress'));
+  done = getObservable(this.store.collection('done'));
+  constructor(private dialog: MatDialog, private store: AngularFirestore) { }
 
   ngOnInit(): void {
   }
+
+
 
   drop(event: CdkDragDrop<Task[]>): void {
     if (event.previousContainer === event.container) {
       return;
     }
+    const item = event.item.data;
+    this.store.firestore.runTransaction(() => {
+      return Promise.all([
+        this.store.collection(event.previousContainer.id).doc(item.id).delete(),
+        this.store.collection(event.container.id).add(item)
+      ]);
+    });
+
     transferArrayItem(
       event.previousContainer.data,
       event.container.data,
@@ -33,7 +51,23 @@ export class TasksComponent implements OnInit {
     );
   }
 
-  edit(list: string, task: Task): void {
+  edit(list: 'done' | 'todo' | 'inProgress', task: Task): void {
+    const dialoRef = this.dialog.open(TaskDialogComponent, {
+      width: '400px',
+      data: {
+        task,
+        enableDelete: true
+      }
+    });
+
+    dialoRef.afterClosed().subscribe((result: TaskDialogResult) => {
+
+      if (result.delete) {
+        this.store.collection(list).doc(task.id).delete();
+      } else {
+        this.store.collection(list).doc(task.id).update(task);
+      }
+    });
 
   }
 
@@ -45,7 +79,7 @@ export class TasksComponent implements OnInit {
       }
     });
     dialogRef.afterClosed()
-    .subscribe((result: TaskDialogResult) => this.todo.push(result.task));
+    .subscribe((result: TaskDialogResult) => this.store.collection('todo').add(result.task));
   }
 
 }
