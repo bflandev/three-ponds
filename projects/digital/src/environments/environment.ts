8860cd237b9e2899306dc0275975252cb5cefd7b
Brian Flanagan <brianflanagan@Brians-MacBook-Pro-2.local>
2021-11-04 21:48:17 -0400

    stashing

--- projects/digital/src/environments/environment.ts
+++ projects/digital/src/environments/environment.ts
@@ -12,8 +12,8 @@ export const environment = {
     storageBucket: 'three-ponds-digital.appspot.com',
     messagingSenderId: '479904043239',
     appId: '1:479904043239:web:d911aa08757e5a87735242',
-    measurementId: 'G-LCZMKBCVQR',
-  },
+    measurementId: 'G-LCZMKBCVQR'
+  }
 };
 
 /*
@@ -23,4 +23,4 @@ export const environment = {
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
  */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
