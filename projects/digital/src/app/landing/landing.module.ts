3a4ad3ce174aad4643181cf2b87fab2767365207
Brian Flanagan <brianflanagan@Brians-MacBook-Pro-2.local>
2021-11-04 21:51:42 -0400

    update material

--- projects/digital/src/app/landing/landing.module.ts
+++ projects/digital/src/app/landing/landing.module.ts
@@ -7,16 +7,17 @@ import { VendorsModule } from 'projects/vendors/src/public-api';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ContactModule } from '../contact/contact.module';
 import { CoreModule } from '../core/core.module';
-
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [HeroComponent],
   imports: [
     CommonModule,
+    RouterModule,
     LandingRoutingModule,
     VendorsModule,
     ContactModule,
     CoreModule
   ]
 })
-export class LandingModule { }
+export class LandingModule {}
